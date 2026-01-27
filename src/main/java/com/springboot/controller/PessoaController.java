e7a1e4c386db79c077d70a4c908cd40e7a9b6efd
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2026-01-27 09:53:58 -0300

    Criando um filtro pelo nome das pessoas

--- src/main/java/com/springboot/controller/PessoaController.java
+++ src/main/java/com/springboot/controller/PessoaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.springboot.model.Pessoa;
@@ -74,6 +75,15 @@ public class PessoaController {
 
 		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
 		modelAndView.addObject("pessoas", pessoaRepository.findAll());
+		modelAndView.addObject("pessoaobj", new Pessoa());
+		return modelAndView;
+	}
+	
+	@PostMapping(value = "/pesquisarpessoa")
+	public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa) {
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		modelAndView.addObject("pessoas", pessoaRepository.findPessoaByName(nomepesquisa));
 		modelAndView.addObject("pessoaobj", new Pessoa());
 		return modelAndView;
 	}
--- src/main/java/com/springboot/repository/PessoaRepository.java
+++ src/main/java/com/springboot/repository/PessoaRepository.java
@@ -1,5 +1,8 @@
 package com.springboot.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +14,6 @@ import jakarta.transaction.Transactional;
 @Transactional
 public interface PessoaRepository extends CrudRepository<Pessoa, Long>{
 
+	@Query("select p from Pessoa p where p.nome like %?1%")
+	List<Pessoa> findPessoaByName(String nome);
 }
