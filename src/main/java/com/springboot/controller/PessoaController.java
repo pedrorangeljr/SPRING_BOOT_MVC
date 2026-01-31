86177bf68b1248424a99fc5871baeeb6bf3ed664
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2026-01-31 09:11:56 -0300

    Exibindo os telefones cadastrado para a pessoa

--- src/main/java/com/springboot/controller/PessoaController.java
+++ src/main/java/com/springboot/controller/PessoaController.java
@@ -100,6 +100,7 @@ public class PessoaController {
 
 		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
 		modelAndView.addObject("pessoaobj", pessoa.get());
+		modelAndView.addObject("telefones", telefoneRepository.getTelefones(idpessoa));
 		return modelAndView;
 	}
 	
@@ -112,6 +113,7 @@ public class PessoaController {
 		
 		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
 		modelAndView.addObject("pessoaobj", pessoa);
+		modelAndView.addObject("telefones", telefoneRepository.getTelefones(pessoaid));
 		return modelAndView;
 	}
 }
--- src/main/java/com/springboot/repository/TelefoneRepository.java
+++ src/main/java/com/springboot/repository/TelefoneRepository.java
@@ -1,5 +1,8 @@
 package com.springboot.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +14,6 @@ import jakarta.transaction.Transactional;
 @Transactional
 public interface TelefoneRepository extends CrudRepository<Telefone, Long>{
 
-	
+	@Query("select t from Telefone t where t.pessoa.id = ?1")
+	public List<Telefone> getTelefones(Long pessoaid);
 }
